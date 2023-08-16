a09b10a84ea1dd2e6fd13d325c4e56aa7648de25
rami jawadi <ramijawadi104@gmail.com>
2023-08-16 10:52:45 +0100

    add immer libraray to optimaze the code

--- nestedState.js
+++ nestedState.js
@@ -1,15 +1,18 @@
 
 
+const { Immer } = require('immer');
 const redux = require('redux');
 const createStore = redux.createStore;
+const producer = require('immer').produce
+
 const initialState = {
 
-    name : 'rami' ,
-    adress : {
+    name: 'rami',
+    adress: {
 
-        street : 'monastir 2000',
-        city:'hay 10',
-        state : 'single'
+        street: 'monastir 2000',
+        city: 'hay 10',
+        state: 'single'
     }
 }
 
@@ -18,40 +21,43 @@ const UPDTAE_STREET = 'UPDTAE_STREET'
 
 
 //action creator
-function updateStreet (street) {
+function updateStreet(street) {
 
     return {
-type:UPDTAE_STREET,
-payload: street ,
+        type: UPDTAE_STREET,
+        payload: street,
     }
 }
- 
+
 //create reducer 
 
-const reducer = (state = initialState , action) => {
+const reducer = (state = initialState, action) => {
 
-switch(action.type){
-case  UPDTAE_STREET :  
-return{
-    ...state,
-    adress:{
-    ...state.adress,
-    street : action.payload,
-},
-}
-default:{
-return state
-} 
-}
+    switch (action.type) {
+        case UPDTAE_STREET:
+            // return{
+            //     ...state,
+            //     adress:{
+            //     ...state.adress,
+            //     street : action.payload,
+            // },
+
+            return producer(state, (draft) => {
+                draft.adress.street = action.payload
+            })
+            default: {
+            return state
+        }
+    }
 }
 const store = createStore(reducer)
-console.log('initial state' , store.getState())
+console.log('initial state', store.getState())
 
-const unsubscribe = store.subscribe(()=>{
-console.log('updated state' , store.getState());
+const unsubscribe = store.subscribe(() => {
+    console.log('updated state', store.getState());
 })
 
-store.dispatch(updateStreet('sousse 2100'));
+store.dispatch(updateStreet('sidi bouzid  6100'));
 
 unsubscribe()
 
