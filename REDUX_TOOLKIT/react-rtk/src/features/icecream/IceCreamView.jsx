125b2adb61ff9927dbd1b2b1f19783539e8a36c9
rami jawadi <ramijawadi104@gmail.com>
2023-08-19 10:14:34 +0100

    add an input value to add to restoked iceCream value

--- REDUX_TOOLKIT/react-rtk/src/features/icecream/IceCreamView.jsx
+++ REDUX_TOOLKIT/react-rtk/src/features/icecream/IceCreamView.jsx
@@ -1,15 +1,19 @@
-
+import { useState } from 'react'
 import { useSelector , useDispatch } from 'react-redux'
 import {iceCremeOrder , iceCremeRestock} from './icecreamSlice'
 
 const IceCreamView = () => {
+  const [value , setValue]= useState(1)
 const numOfCream = useSelector((state)=>state.cream.numOfCream )
 const dispatch = useDispatch()
   return (
     <div>
     <h1> number of ice Cream  {numOfCream}</h1>
-    <button onClick={()=>dispatch(iceCremeOrder())} > order ice Cream</button>
-    <button onClick={()=>dispatch(iceCremeRestock(5))}> restock ice Creams </button>
+    <button onClick={()=>dispatch(iceCremeOrder())} > order ice Cream</button> <br /><br />
+
+    <input type='number' value={value} onChange={(e)=>setValue(parseInt (e.target.value))} />
+
+    <button onClick={()=>dispatch(iceCremeRestock(value))}> restock ice Creams </button>
         </div>
   )
 }
